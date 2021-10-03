fc989d5d8c171e6aafc8b10b4c369b4ae521ee43
Laura Endreß <laura.endress@syntaxphoenix.com>
2021-10-03 03:04:36 +0200

    Add basics

---
+++ src/main/java/org/playuniverse/minecraft/core/lithos/Lithos.java
@@ -0,0 +1,29 @@
+package org.playuniverse.minecraft.core.lithos;
+
+import java.io.File;
+
+import org.pf4j.PluginWrapper;
+import org.playuniverse.minecraft.mcs.spigot.plugin.SpigotCorePlugin;
+
+public final class Lithos extends SpigotCorePlugin {
+
+    public Lithos(PluginWrapper wrapper, File dataLocation) {
+        super(wrapper, dataLocation);
+    }
+
+    @Override
+    protected void onStart() {
+
+    }
+
+    @Override
+    protected void onServerReady() {
+
+    }
+
+    @Override
+    protected void onStop() {
+
+    }
+
+}
