d05e3a05a31f7086d55ac14530a05fb9a8979a53
Laura Endreß <laura.endress@syntaxphoenix.com>
2021-10-03 11:17:33 +0200

    Try to get test command to work

--- src/main/java/org/playuniverse/minecraft/core/lithos/Lithos.java
+++ src/main/java/org/playuniverse/minecraft/core/lithos/Lithos.java
@@ -3,7 +3,6 @@ package org.playuniverse.minecraft.core.lithos;
 import java.io.File;
 
 import org.pf4j.PluginWrapper;
-import org.playuniverse.minecraft.core.lithos.extension.ICommandExtension;
 import org.playuniverse.minecraft.mcs.spigot.plugin.SpigotCorePlugin;
 
 public final class Lithos extends SpigotCorePlugin {
@@ -11,20 +10,10 @@ public final class Lithos extends SpigotCorePlugin {
     public Lithos(PluginWrapper wrapper, File dataLocation) {
         super(wrapper, dataLocation);
     }
-
+    
     @Override
     protected void onStart() {
-        ICommandExtension.register(this);
-    }
-
-    @Override
-    protected void onServerReady() {
-
-    }
-
-    @Override
-    protected void onStop() {
-
+        
     }
 
 }
--- src/main/java/org/playuniverse/minecraft/core/lithos/command/TestCommand.java
+++ src/main/java/org/playuniverse/minecraft/core/lithos/command/TestCommand.java
@@ -1,11 +1,12 @@
 package org.playuniverse.minecraft.core.lithos.command;
 
 import org.pf4j.Extension;
-import org.playuniverse.minecraft.core.lithos.extension.ICommandExtension;
 import org.playuniverse.minecraft.mcs.spigot.command.CommandContext;
 import org.playuniverse.minecraft.mcs.spigot.command.listener.MinecraftInfo;
 import org.playuniverse.minecraft.mcs.spigot.command.nodes.CommandNode;
 import org.playuniverse.minecraft.mcs.spigot.command.nodes.RootNode;
+import org.playuniverse.minecraft.mcs.spigot.plugin.extension.ICommandExtension;
+import org.playuniverse.minecraft.mcs.spigot.plugin.extension.info.CommandInfo;
 
 @Extension
 public final class TestCommand implements ICommandExtension {
