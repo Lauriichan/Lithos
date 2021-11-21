0bc6e518997fc187b6accd76ec117d732d31b688
Laura Endreß <laura.endress@syntaxphoenix.com>
2021-11-21 11:09:38 +0100

    Remove Shop and Add Trading stuff

---
+++ src/main/java/org/playuniverse/minecraft/core/lithos/feature/trade/TradeHandler.java
@@ -0,0 +1,13 @@
+package org.playuniverse.minecraft.core.lithos.feature.trade;
+
+import org.playuniverse.minecraft.core.lithos.feature.Feature;
+
+public final class TradeHandler extends Feature {
+
+    @Override
+    protected void onTick(long deltaTime) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
